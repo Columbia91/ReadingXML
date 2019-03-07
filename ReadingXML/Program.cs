5190afd8e53cd100654eec434429be7be4fe342d
cougar91 <alpha.z91@mail.ru>
2019-03-07 16:13:02 +0600

    change_

--- ReadingXML/Program.cs
+++ ReadingXML/Program.cs
@@ -12,19 +12,18 @@ namespace ReadingXML
     {
         static void Main(string[] args)
         {
-            XDocument doc = XDocument.Load("cafe.xml");
-
-            foreach (XElement foodElement in doc.Element("breakfast_menu").Elements("food"))
+            XDocument xdoc = XDocument.Load("cafe.xml");
+            Console.WriteLine(xdoc.ToString());
+           
+            foreach (XElement foodElement in xdoc.Element("Breakfast_menu").Elements("Food"))
             {
-                XAttribute nameAttribute = foodElement.Attribute("name");
+                XElement companyElement = foodElement.Element("name");
                 XElement priceElement = foodElement.Element("price");
-                XElement descriptionElement = foodElement.Element("description");
 
-                if (nameAttribute != null && priceElement != null && descriptionElement != null)
+                if (companyElement != null && priceElement != null)
                 {
-                    Console.WriteLine("Смартфон: {0}", nameAttribute.Value);
+                    Console.WriteLine("Компания: {0}", companyElement.Value);
                     Console.WriteLine("Цена: {0}", priceElement.Value);
-                    Console.WriteLine("Описание: {0}", descriptionElement.Value);
                 }
                 Console.WriteLine();
             }
@@ -33,36 +32,3 @@ namespace ReadingXML
 }
 /*1.	С помощью класса XDocument прочитать XML файл и вывести название всех его тегов с сохранением иерархии. 
  * В качестве отступов для иерархии использовать 2 пробела.*/
-
-/* private void button1_Click(object sender, EventArgs e)
-    {
-
-        string s = "";
-
-        s = textBox1.Text; // ввожу текст (Немецкий язык)
-
-        if (s != "") // файл , и тест поиска не должен быть пустым
-        {
-
-
-            string fileName = "lang.xml";
-            //читаем данные из файла
-            XDocument doc = XDocument.Load(fileName);
-            foreach (XElement el in doc.Root.Elements())
-            {
-                foreach (XAttribute attr in el.Attributes())
-                {
-                    if (attr.Value == s)
-                    {
-                        //MessageBox.Show(el.Name.LocalName);
-                        textBox2.Text = el.Name.LocalName;
-                    }
-
-
-                }
-
-            }
-
-
-
-        }*/
